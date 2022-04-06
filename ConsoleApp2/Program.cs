06f847a64493c9938fbf1c43e1f563e4318f03a0
Nguyen Xuan Giao <giao5fs@outlook.com>
2022-04-07 01:18:08 +0700

    first push2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var res = RemoteFileExists(@"https://tools.l1cache.pixelz.com/api/Image/2/2/o%2F58%2F11%2F52%2F31%2");
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+        ///
+        /// Checks the file exists or not.
+        ///
+        /// The URL of the remote file.
+        /// True : If the file exits, False if file not exists
+        static bool RemoteFileExists(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "GET";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TRUE if the Status code == 200
+                var res = response.StatusCode == HttpStatusCode.OK;
+                response.Close();
+                return res;
+            }
+            catch
+            {
+                //Any exception will returns false.
+                return false;
+            }
+        }
+    }
+}
